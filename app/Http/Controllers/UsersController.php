71ad316a707eb4ac8d5b89af5d1c4591d7360184
Kousik Satish <kousiksatish@gmail.com>
2016-03-02 12:58:55 +0530

    GCM and Device checks added while registering

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Users as Users;
 
 class UsersController extends Controller
 {
@@ -41,11 +42,38 @@ class UsersController extends Controller
         $username = $request->username;
         $password = $request->password;
         $GCM_Id = $request->gcmid;
-        $AD_Id = $request->AD_Id;
+        $AD_Id = $request->ad_id;
         $shellcmd = "python2 nitt_imap_login.py ".$username." ".$password;
         $imap = shell_exec($shellcmd);
         if($imap == 1)
         {   
+            /*(1) Same Username, GCMId, AdId */
+            $user_exists = Users::where('Username', $username)
+                                ->where('GCM_ID', $GCM_Id)
+                                ->where('Ad_ID', $AD_Id)
+                                ->first();
+            if ($user_exists)
+                return response()->json(['status' => '200', 'data' => ['description' => 'Successfully authenticated!']]);
+            /*(1) Ends withiout adding anything in DB*/
+
+            /*(2) Same AdId or Same Device*/
+            $user_device_exists = Users::where('Ad_ID', $AD_Id)->first();
+            if($user_device_exists)
+            {
+                $user_device_exists->GCM_Id = $GCM_Id;
+                $user_device_exists->Username = $username;
+                $user_device_exists->save();
+                return response()->json(['status' => '200', 'data' => ['description' => 'Successfully authenticated!']]);
+            }
+            /*(2) Ends modifying GCM_Id and Username for the devide */
+            
+            $user = new Users();
+            $user->Username = $username;
+            $user->GCM_ID = $GCM_Id;
+            $user->AD_ID = $AD_Id;
+            $user->path = "btech/cse/14";
+            $user->save();
+
             return response()->json(['status' => '200', 'data' => ['description' => 'Successfully authenticated!']]);
         }
         else
